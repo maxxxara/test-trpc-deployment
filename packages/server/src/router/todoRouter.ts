9d793afdada337eb70450ee5799f996f1f9fa210
maxxxara <dati.maxarashvili24@gmail.com>
2025-03-21 18:07:44 +0400

    prisma, postgre, postgre docker

--- packages/server/src/router/todoRouter.ts
+++ packages/server/src/router/todoRouter.ts
@@ -3,23 +3,20 @@ import data from "../data.json";
 import { z } from "zod";
 import * as fs from "fs";
 import * as path from "path";
-
+import { prisma } from "../lib/prisma";
 export const todoRouter = trpc.router({
   getTodos: trpc.procedure.query(async () => {
-    return data;
+    return await prisma.todo.findMany();
   }),
   createTodo: trpc.procedure.input(z.object({
     title: z.string(),
   })).mutation(async ({ input }) => {
-    const newTodo = {
-      id: Math.random().toString(),
-      title: input.title,
-      completed: false,
-    };
-    data.push(newTodo);
-    const filePath = path.resolve(__dirname, "../data.json");
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-    return newTodo;
+    await prisma.todo.create({
+      data: {
+        title: input.title,
+        isCompleted: false,
+      }
+    });
   }),
 });
 
